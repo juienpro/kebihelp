558a5249a06b0c9d96033749481da1ac5bbcc623
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-06-21 16:08:43 -0400

    CONFIG_FILE change

--- src/kebihelp/config.py
+++ src/kebihelp/config.py
@@ -6,7 +6,7 @@ from prettytable import PrettyTable
 # import libs.Group as Group
 # import libs.Shortcut as Shortcut
 
-CONFIG_FILE = "~/.config/kebihelp.json"
+CONFIG_FILE = "~/.config/kebihelp/conf.json"
 
 class Config():
     def __init__(self):
